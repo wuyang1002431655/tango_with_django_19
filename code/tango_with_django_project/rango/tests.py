79927675a56bf4e2acb1220b6819296d0c9a02a2
leifos <leifos@acm.org>
2016-04-24 18:55:25 +0100

    restructured the tests

--- code/tango_with_django_project/rango/tests.py
+++ code/tango_with_django_project/rango/tests.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.contrib.staticfiles import finders
 
 # Create your tests here.
-class Chapter4ViewTests(TestCase):
+class GeneralTests(TestCase):
+    def test_serving_static_files(self):
+        # If using static media properly result is not NONE once it finds rango.jpg
+        result = finders.find('images/rango.jpg')
+        self.assertIsNotNone(result)
+
+
+class IndexPageTests(TestCase):
         
     def test_index_contains_hello_message(self):
         # Check if there is the message 'hello world!'
@@ -20,7 +28,16 @@ class Chapter4ViewTests(TestCase):
 
         # Check if is there an image in index page
         self.assertIn('img src="/static/images/rango.jpg', response.content)
+    
+    def test_index_has_title(self):
+        response = self.client.get(reverse('index'))
+        #Check to make sure that the title tag has been used 
+        self.assertIn('<title>', response.content)
+        self.assertIn('</title>', response.content)
 
+
+class AboutPageTests(TestCase):
+        
     def test_about_contains_create_message(self):
         # Check if in the about page is there a message
         response = self.client.get(reverse('about'))
@@ -31,8 +48,3 @@ class Chapter4ViewTests(TestCase):
 
         # Check the template used to render index page
         self.assertTemplateUsed(response, 'rango/about.html')
-        
-        
-
-        
-        
