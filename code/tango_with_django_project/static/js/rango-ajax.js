8972b0423c362c1decf4b664dab4b216217fd88d
David Maxwell <maxwelld90@gmail.com>
2016-09-25 19:16:41 +1000

    Bleep

--- code/tango_with_django_project/static/js/rango-ajax.js
+++ code/tango_with_django_project/static/js/rango-ajax.js
@@ -30,35 +30,5 @@
 	                   me.hide();
 	               });
 	    });
-    
-    //	Edit profile
-    	$('#edit').click(function(){
-    		$.get('/rango/edit_profile/', {}, function(data){
-    			$('#edit_profile').html(data).ready(function(){
-    				$('#cancel').click(function(){
-    					location.reload()
-    				});
-    			});
-    		$('#profile_data').hide();
-    		});
-    	});
-
-    //	Change Password
-    	$('#change_password').click(function(){
-    		$.get('/rango/change_password/', {}, function(data){
-    			$('#change_password_div').html(data).ready(function(){
-    				$('#cancel_password').click(function(){
-    					location.reload()
-    				});
-    			});
-    		$('#profile_data').hide();
-    		});
-    	});
-
-    	$('#cancel_password').click(function() {
-    		window.location.replace(window.location.href);
-    	});
-
-
 
 	});
