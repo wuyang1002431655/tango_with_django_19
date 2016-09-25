2057157c4386ea94033bd6dbe1500286ca9414de
leifos <leifos@acm.org>
2016-09-25 08:49:23 +0100

    updating tests

--- code/tango_with_django_project/rango/tests.py
+++ code/tango_with_django_project/rango/tests.py
@@ -130,9 +130,50 @@ class Chapter4ViewTests(TestCase):
 
 class Chapter5ViewTests(TestCase):
 
+    def setUp(self):
+        try:
+            from populate_rango import populate
+            populate()
+        except ImportError:
+            print('The module populate_rango does not exist')
+        except NameError:
+            print('The function populate() does not exist or is not correct')
+        except:
+            print('Something went wrong in the populate() function :-(')
+
+
+    def get_category(self, name):
+
+        from rango.models import Category
+        try:
+            cat = Category.objects.get(name=name)
+        except Category.DoesNotExist:
+            cat = None
+        return cat
+
+    def test_python_cat_added(self):
+        cat = self.get_category('Python')
+        self.assertIsNotNone(cat)
+
+    def test_python_cat_with_views(self):
+        cat = self.get_category('Python')
+
+        self.assertEquals(cat.views, 128)
+
+    def test_python_cat_with_likes(self):
+        cat = self.get_category('Python')
+        self.assertEquals(cat.likes, 64)
+
     def test_view_has_title(self):
         response = self.client.get(reverse('index'))
 
         #Check title used correctly
         self.assertIn('<title>', response.content)
         self.assertIn('</title>', response.content)
+
+    # Need to add tests to:
+    # check admin interface - is it configured and set up
+
+    def test_admin_interface_page_view(self):
+        from admin import PageAdmin
+        self.assertIn('category', PageAdmin.list_display)
